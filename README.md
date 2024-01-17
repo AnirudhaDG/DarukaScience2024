22c42b9e1caa1ac134e70188823dbfa2692fb6ae
eccentricOrange <eccentric.orange2@gmail.com>
2024-01-17 11:34:58 +0530

    documented

---
+++ README.md
@@ -0,0 +1,76 @@
+# Science Sensor Array Firmware
+
+This repo runs all the science sensors used in our sensor array.
+
+## Sensors
+
+### LTR390 Adafruit UV Sensor
+* Product page: https://www.adafruit.com/product/4831
+* Adafruit tutorial: https://learn.adafruit.com/adafruit-ltr390-uv-sensor
+* Datasheet: https://optoelectronics.liteon.com/upload/download/DS86-2015-0004/LTR-390UV_Final_%20DS_V1%201.pdf
+* Protocol: I2C
+* Voltage: 5V
+* Library: https://github.com/levkovigor/LTR390.git
+
+### BME280 Temperature, Humidity, Pressure Sensor
+* Product page: https://www.bosch-sensortec.com/products/environmental-sensors/humidity-sensors-bme280/
+* Datasheet: https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme280-ds002.pdf
+* Protocol: I2C
+* Voltage: 5V
+* Library: https://github.com/finitespace/BME280.git
+
+### AGS02MA Air Quality Sensor
+* Product page: http://www.aosong.com/m/en/products-33.html
+* Datasheet: https://cdn-shop.adafruit.com/product-files/5593/datasheet+ags02ma.pdf
+* Protocol: I2C
+* Voltage: **3.3V**
+* Library: https://github.com/RobTillaart/AGS02MA.git
+
+### MQ-9 Gas Sensor (Carbon Monoxide, Methane, LPG)
+* Product page: https://robu.in/product/mq-9-carbon-monoxide-methane-lpg-gas-sensor-module/
+* Datasheet: https://robu.in/wp-content/uploads/2017/09/MQ-9-Datasheet-ROBU.IN_.pdf
+* Protocol: Analog
+* Voltage: 5V
+
+### MQ-136 Gas Sensor (Hydrogen Sulfide)
+* Product page: https://robu.in/product/mq-136-hydrogen-sulfide-gas-sensor-module/
+* Datasheet: http://www.sensorica.ru/pdf/MQ-136.pdf
+* Protocol: Analog
+* Voltage: 5V
+
+### NPK Soil Sensor
+* Library: https://github.com/4-20ma/ModbusMaster.git
+* Protocol: RS485
+* Voltage: 12V
+* Datasheet: [attached](./npk-datasheet.pdf)
+
+### MAX485 RS485 Module (for NPK Soil Sensor)
+* IC product page: https://www.analog.com/en/products/max485.html
+* Module product page: https://robu.in/product/max485-ttl-rs485/
+* Datasheet: https://datasheets.maximintegrated.com/en/ds/MAX1487-MAX491.pdf
+* Protocol: RS485/TTL
+* Voltage: 5V
+
+## Data format
+The data is returned on a Serial port every 20 seconds. It is formatted as a comma-separated string.
+
+Sequence of data:
+
+| Index | Sensor | Data Type | Units |
+| --- | --- | --- | --- |
+| 1 | BME280 | Temperature | °C |
+| 2 | BME280 | Humidity | % |
+| 3 | BME280 | Pressure | Pa |
+| 4 | BME280 | Altitude | m |
+| 5 | AGS02MA | TVOC | ppb |
+| 6 | LTR390 | Value type | 0 = Ambient Light, 1 = UV Index |
+| 7 | LTR390 | Value | lux or UV index |
+| 8 | MQ-9 | Value | (analogue; 0-1023) |
+| 9 | MQ-136 | Value | (analogue; 0-1023) |
+| 10 | NPK | Temperature | °C |
+| 11 | NPK | Moisture | % |
+| 12 | NPK | Electrical Conductivity | µS/cm |
+| 13 | NPK | pH | pH |
+| 14 | NPK | Nitrogen | mg/kg |
+| 15 | NPK | Phosphorus | mg/kg |
+| 16 | NPK | Potassium | mg/kg |
--- science_final/science_final.ino
+++ science_final/science_final.ino
@@ -1,32 +1,43 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <ModbusMaster.h>
-#include <SoftwareSerial.h>
 
-#define FAILING_DELAY 1e3
-#define SERIAL_DELAY 1e2
-#define SENSOR_DELAY 20e3
+#define FAILING_DELAY 1e3  // how long to wait before retrying to initialize a sensor
+#define SENSOR_DELAY 20e3  // how long to wait between sensor readings
+#define NPK_BAUD_RATE 9600
 #define SERIAL_BAUD_RATE 9600
-#define MQ136_PIN A0
-#define MQ9_PIN A1
-
 
 // LTR390 Adafruit UV Sensor
 #include <LTR390.h>
 #define UV_I2C_ADDRESS 0x53
 LTR390 UVSensor = LTR390(UV_I2C_ADDRESS);
 
+void configureUV();
+void readUVData();
+
 // AGS02MA Adafruit Gas Sensor
 #include <AGS02MA.h>
 #define GAS_I2C_ADDRESS 26
 AGS02MA gasSensor = AGS02MA(GAS_I2C_ADDRESS);
 
+void configureAGS02MAGasSensor();
+void readAGS02MAGasSensor();
+
 // BME280 Bosch Environmental Sensor
 #include <BME280I2C.h>
 BME280I2C barometer;
 
-//NPK init
+void configureBME280();
+void readBME280Data();
+
+// Analog Gas Sensors
+uint8_t MQ136_PIN = A0;
+uint8_t MQ9_PIN = A1;
+
+// NPK Sensor
+#include <ModbusMaster.h>
+#include <SoftwareSerial.h>
+
 const uint8_t RO_PIN = 2;
 const uint8_t DI_PIN = 3;
 const uint8_t RE_PIN = 7;
@@ -35,188 +46,181 @@ const uint8_t DE_PIN = 6;
 SoftwareSerial swSerial(RO_PIN, DI_PIN);  // Receive (data in) pin, Transmit (data out) pin
 ModbusMaster NPKObject;
 
-// Put the MAX485 into transmit mode
-void preTransmission() {
-  digitalWrite(RE_PIN, 1);
-  digitalWrite(DE_PIN, 1);
-}
-
-// Put the MAX485 into receive mode
-void postTransmission() {
-  digitalWrite(RE_PIN, 0);
-  digitalWrite(DE_PIN, 0);
-}
-
-void configureNPK() {
-  // configure the MAX485 RE & DE control signals and enable receive mode
-  pinMode(RE_PIN, OUTPUT);
-  pinMode(DE_PIN, OUTPUT);
-  digitalWrite(DE_PIN, 0);
-  digitalWrite(RE_PIN, 0);
-  
-  // Modbus communication runs at 9600 baud
-  swSerial.begin(SERIAL_BAUD_RATE);
-
-  // Modbus slave ID of NPK sensor is 1
-  NPKObject.begin(1, swSerial);
-
-  // Callbacks to allow us to set the RS485 Tx/Rx direction
-  NPKObject.preTransmission(preTransmission);
-  NPKObject.postTransmission(postTransmission);
-}
-
-void readNPK() {
-  uint8_t result;
-  Serial.print("NPK");
-  Serial.print(",");
-
-  // remove any characters from the receive buffer
-  // ask for 7x 16-bit words starting at register address 0x0000
-  result = NPKObject.readHoldingRegisters(0x0000, 7);
-
-  if (result == NPKObject.ku8MBSuccess) {
-    Serial.print(NPKObject.getResponseBuffer(0) / 10.0);  //temprature in C
-    Serial.print(",");
-
-    Serial.print(NPKObject.getResponseBuffer(1) / 10.0);  //moisture %
-    Serial.print(",");
-
-    Serial.print(NPKObject.getResponseBuffer(2));  //EC in uS/cm
-    Serial.print(",");
-
-    Serial.print(NPKObject.getResponseBuffer(3) / 100.0);  //pH
-    Serial.print(",");
-
-    Serial.print(NPKObject.getResponseBuffer(4) * 10);  // N in mg/kg
-    Serial.print(",");
-
-    Serial.print(NPKObject.getResponseBuffer(5) * 10);  // P in mg/kg
-    Serial.print(",");
-
-    Serial.print(NPKObject.getResponseBuffer(6) * 10);  // K in mg/kg
-    Serial.print(",");
-  } else {
-    Serial.println("Error");
-    Serial.print(",");
-  }
-}
-
-
-// BME280 Bosch Environmental Sensor
-void configureBME280() {
-  delay(SERIAL_DELAY);
-  while (!barometer.begin()) {
-    delay(FAILING_DELAY);
-  }
-}
-
-void readBME280Data() {
-  float temp(NAN), hum(NAN), pres(NAN);
-
-  BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
-  BME280::PresUnit presUnit(BME280::PresUnit_Pa);
-
-  barometer.read(pres, temp, hum, tempUnit, presUnit);
-
-  Serial.print("BME280");
-  Serial.print(",");
-  Serial.print(temp);
-  Serial.print(",");
-  Serial.print(hum);
-  Serial.print(",");
-  Serial.print(pres);
-  Serial.print(",");  // in Pa
-
-  float altitude = 44330 * (1.0 - pow((pres / 100) / 1013, 0.1903));
-  Serial.print(altitude);
-  Serial.print(",");;
-}
-
-// AGS02MA Adafruit Gas Sensor
-void configureAGS02MAGasSensor() {
-
-  delay(SERIAL_DELAY);
-
-  while (!gasSensor.begin()) {
-    delay(FAILING_DELAY);
-  }
-
-  gasSensor.setPPBMode();
-}
-
-void readAGS02MAGasSensor() {
-  Serial.print("AGS02MA");
-  Serial.print(",");
-  Serial.print(gasSensor.readPPB());
-  Serial.print(",");
-}
-
-// Analog Gas Sensor
-void readGasAnalogSensor(uint8_t pin) {
-  long value = analogRead(pin);
-  Serial.print(value);
-  Serial.print(",");
-}
-
-// LTR390 Adafruit UV Sensor
-void configureUV() {
-  delay(SERIAL_DELAY);
-
-  while (!UVSensor.init()) {
-    delay(FAILING_DELAY);
-  }
-  UVSensor.setMode(LTR390_MODE_ALS);
-  UVSensor.setGain(LTR390_GAIN_3);
-  UVSensor.setResolution(LTR390_RESOLUTION_18BIT);
-}
-
-void readUVData() {
-  if (UVSensor.newDataAvailable()) {
-    Serial.print("MQ136");
-    Serial.print(",");
-    if (UVSensor.getMode() == LTR390_MODE_ALS) {
-      Serial.print("0.0");
-      Serial.print(",");
-      Serial.println(UVSensor.getLux());
-      UVSensor.setGain(LTR390_GAIN_18);                 // Recommended for UVI - x18
-      UVSensor.setResolution(LTR390_RESOLUTION_20BIT);  // Recommended for UVI - 20-bit
-      UVSensor.setMode(LTR390_MODE_UVS);
-    } else if (UVSensor.getMode() == LTR390_MODE_UVS) {
-      Serial.print("1.0");
-      Serial.print(",");
-      Serial.println(UVSensor.getUVI());
-      UVSensor.setGain(LTR390_GAIN_3);                  // Recommended for Lux - x3
-      UVSensor.setResolution(LTR390_RESOLUTION_18BIT);  // Recommended for Lux - 18-bit
-      UVSensor.setMode(LTR390_MODE_ALS);
-    }
-  }
-}
+void preTransmission();
+void postTransmission();
+void configureNPK();
+void readNPK();
 
 void setup() {
-  Wire.begin();
+    Wire.begin();
 
-  Serial.begin(SERIAL_BAUD_RATE);
+    Serial.begin(SERIAL_BAUD_RATE);
 
-  while (!Serial) {
-  }  // Wait
-  configureBME280();
-  configureAGS02MAGasSensor();
-  configureUV();
-  configureNPK();
+    while (!Serial) {
+    }  // Wait
+    configureBME280();
+    configureAGS02MAGasSensor();
+    configureUV();
+    configureNPK();
 }
 
 int counter = 0;
 unsigned long sensorTimer = millis();
 
 void loop() {
-  if (millis() >= sensorTimer + SENSOR_DELAY) {
-    readBME280Data();
-    readAGS02MAGasSensor();
-    readUVData();
-    readGasAnalogSensor(MQ9_PIN);
+    if (millis() >= sensorTimer + SENSOR_DELAY) {
 
-    readGasAnalogSensor(MQ136_PIN);
-    readNPK();
-    sensorTimer = millis();
-  }
+        // DO NOT CHANGE THE ORDER OF THESE FUNCTIONS
+        readBME280Data();
+        readAGS02MAGasSensor();
+        readUVData();
+        readGasAnalogSensor(MQ9_PIN);
+        readGasAnalogSensor(MQ136_PIN);
+        readNPK();
+        sensorTimer = millis();
+    }
+}
+
+// Put the MAX485 into transmit mode
+void preTransmission() {
+    digitalWrite(RE_PIN, 1);
+    digitalWrite(DE_PIN, 1);
+}
+
+// Put the MAX485 into receive mode
+void postTransmission() {
+    digitalWrite(RE_PIN, 0);
+    digitalWrite(DE_PIN, 0);
+}
+
+void configureNPK() {
+    // configure the MAX485 RE & DE control signals and enable receive mode
+    pinMode(RE_PIN, OUTPUT);
+    pinMode(DE_PIN, OUTPUT);
+    digitalWrite(DE_PIN, 0);
+    digitalWrite(RE_PIN, 0);
+
+    // Modbus communication runs at 9600 baud
+    swSerial.begin(NPK_BAUD_RATE);
+
+    // Modbus slave ID of NPK sensor is 1
+    NPKObject.begin(1, swSerial);
+
+    // Callbacks to allow us to set the RS485 Tx/Rx direction
+    NPKObject.preTransmission(preTransmission);
+    NPKObject.postTransmission(postTransmission);
+}
+
+void readNPK() {
+    uint8_t result;
+    Serial.print("NPK");
+    Serial.print(",");
+
+    // remove any characters from the receive buffer
+    // ask for 7x 16-bit words starting at register address 0x0000
+    result = NPKObject.readHoldingRegisters(0x0000, 7);
+
+    if (result == NPKObject.ku8MBSuccess) {
+        Serial.print(NPKObject.getResponseBuffer(0) / 10.0);  // temprature in C
+        Serial.print(",");
+
+        Serial.print(NPKObject.getResponseBuffer(1) / 10.0);  // moisture %
+        Serial.print(",");
+
+        Serial.print(NPKObject.getResponseBuffer(2));  // EC in uS/cm
+        Serial.print(",");
+
+        Serial.print(NPKObject.getResponseBuffer(3) / 100.0);  // pH
+        Serial.print(",");
+
+        Serial.print(NPKObject.getResponseBuffer(4) * 10);  // N in mg/kg
+        Serial.print(",");
+
+        Serial.print(NPKObject.getResponseBuffer(5) * 10);  // P in mg/kg
+        Serial.print(",");
+
+        // This sensor has println because it is the last one
+        Serial.println(NPKObject.getResponseBuffer(6) * 10);  // K in mg/kg
+    } else {
+        Serial.println("Error");
+    }
+}
+
+// BME280 Bosch Environmental Sensor
+void configureBME280() {
+    while (!barometer.begin()) {
+        delay(FAILING_DELAY);
+    }
+}
+
+void readBME280Data() {
+    float temp(NAN), hum(NAN), pres(NAN);
+
+    BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+    BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+
+    barometer.read(pres, temp, hum, tempUnit, presUnit);
+
+    Serial.print(temp);
+    Serial.print(",");
+    Serial.print(hum);
+    Serial.print(",");
+    Serial.print(pres);
+    Serial.print(",");  // in Pa
+
+    float altitude = 44330 * (1.0 - pow((pres / 100) / 1013, 0.1903));
+    Serial.print(altitude);
+    Serial.print(",");
+    ;
+}
+
+// AGS02MA Adafruit Gas Sensor
+void configureAGS02MAGasSensor() {
+    while (!gasSensor.begin()) {
+        delay(FAILING_DELAY);
+    }
+
+    gasSensor.setPPBMode();
+}
+
+void readAGS02MAGasSensor() {
+    Serial.print(gasSensor.readPPB());
+    Serial.print(",");
+}
+
+// Analog Gas Sensor
+void readGasAnalogSensor(uint8_t pin) {
+    long value = analogRead(pin);
+    Serial.print(value);
+    Serial.print(",");
+}
+
+// LTR390 Adafruit UV Sensor
+void configureUV() {
+    while (!UVSensor.init()) {
+        delay(FAILING_DELAY);
+    }
+    UVSensor.setMode(LTR390_MODE_ALS);
+    UVSensor.setGain(LTR390_GAIN_3);
+    UVSensor.setResolution(LTR390_RESOLUTION_18BIT);
+}
+
+void readUVData() {
+    if (UVSensor.newDataAvailable()) {
+        if (UVSensor.getMode() == LTR390_MODE_ALS) {
+            Serial.print("0.0");
+            Serial.print(",");
+            Serial.println(UVSensor.getLux());
+            UVSensor.setGain(LTR390_GAIN_18);                 // Recommended for UVI - x18
+            UVSensor.setResolution(LTR390_RESOLUTION_20BIT);  // Recommended for UVI - 20-bit
+            UVSensor.setMode(LTR390_MODE_UVS);
+        } else if (UVSensor.getMode() == LTR390_MODE_UVS) {
+            Serial.print("1.0");
+            Serial.print(",");
+            Serial.println(UVSensor.getUVI());
+            UVSensor.setGain(LTR390_GAIN_3);                  // Recommended for Lux - x3
+            UVSensor.setResolution(LTR390_RESOLUTION_18BIT);  // Recommended for Lux - 18-bit
+            UVSensor.setMode(LTR390_MODE_ALS);
+        }
+    }
 }
